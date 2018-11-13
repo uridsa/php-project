157fea3ae1ed6290dbc620704431df0b60bcc7f9
Urid Lugo <uridsa@gmail.com>
2018-11-12 20:13:59 -0600

    modified:   index.php

--- index.php
+++ index.php
@@ -3,4 +3,6 @@
    echo "HOLA ESTE ES EL INDEX EN PHP.. :-)";
    echo "<br>";
    echo "Linea escrita desde openshift.. :-)";
+   echo "<br>";
+   echo "Fecha de ultima modificación.. 12-11-2018 a las 20:15 hrs..";
 ?>
